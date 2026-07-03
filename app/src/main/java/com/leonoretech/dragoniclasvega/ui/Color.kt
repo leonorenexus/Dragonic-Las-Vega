fb28a0d721d4d30f9032369ac4082f9d81602128
Leonorenexus <leonorenexus@gmail.com>
2026-07-03 14:24:58 +0700

    Create Color.kt

---
+++ app/src/main/java/com/leonoretech/dragoniclasvega/ui/Color.kt
@@ -0,0 +1,17 @@
+package com.leonoretech.dragoniclasvegas.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Core cyberpunk palette
+val VoidBlack = Color(0xFF06040C)
+val DeepPurple = Color(0xFF1A0A2E)
+val NeonPurple = Color(0xFFB026FF)
+val NeonBlue = Color(0xFF00E5FF)
+val NeonGold = Color(0xFFFFD700)
+val NeonPink = Color(0xFFFF2E9A)
+val GlassSurface = Color(0x33FFFFFF)
+val GlassBorder = Color(0x66B026FF)
+val TextPrimary = Color(0xFFF4F1FF)
+val TextSecondary = Color(0xFFAFA6CC)
+val DangerRed = Color(0xFFFF3860)
+val SuccessGreen = Color(0xFF39FF88)
